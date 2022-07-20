9902f19140350717df4979ad1655c05a0df6e6a0
Joseph Francis <hookedupjoe@gmail.com>
2022-07-20 09:00:37 -0400

    Version bump

--- actionapp-wp.php
+++ actionapp-wp.php
@@ -27,9 +27,9 @@
  * Text Domain: actapp
  * License: GPLv3
  * 
- * Version: 1.0.2
+ * Version: 1.0.3
  */
-define( 'ACTIONAPP_WP_CORE_VERSION', '1.0.2' );
+define( 'ACTIONAPP_WP_CORE_VERSION', '1.0.3' );
  
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
