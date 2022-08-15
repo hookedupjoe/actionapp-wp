ed49d7a756e4cf005c276e0d840644ba4023f21f
Joseph Francis <hookedupjoe@gmail.com>
2022-08-15 14:48:43 -0400

    format

--- blocks/image.js
+++ blocks/image.js
@@ -18,8 +18,8 @@
  * @since actionappwp 1.0.0
  */
 
-( function ( wp, ActionAppCore ) {
-    
+(function (wp, ActionAppCore) {
+
     var el = wp.element.createElement;
     var useBlockProps = wp.blockEditor.useBlockProps;
     var BlockEditor = ActionAppCore.common.blocks.Editor;
@@ -28,129 +28,122 @@
         name: 'image',
         title: 'UI Image',
         example: {
-            attributes: {color: 'blue',text: 'Image Text', size: 'large'}
+            attributes: { color: 'blue', text: 'Image Text', size: 'large' }
         },
         category: 'actappui',
         atts: {}
     };
     const iconEl = BlockEditor.getControlIcon(info.name);
 
-    BlockEditor.addBooleanAtts(info.atts,['fluid','avatar','rounded','circular','urlopentab','bordered','centered']);
-    BlockEditor.addStringAtts(info.atts,['text','color','size', 'alignmentvertical', 'url', 'mediaURL','float']);
-    BlockEditor.addNumberAtts(info.atts,['mediaID']);
+    BlockEditor.addBooleanAtts(info.atts, ['fluid', 'avatar', 'rounded', 'circular', 'urlopentab', 'bordered', 'centered']);
+    BlockEditor.addStringAtts(info.atts, ['text', 'color', 'size', 'alignmentvertical', 'url', 'mediaURL', 'float']);
+    BlockEditor.addNumberAtts(info.atts, ['mediaID']);
 
     var tmpClassSpecs = {
-        boolean: ['fluid','avatar','rounded','circular','bordered','centered'],
-        string: ['color','size', 'alignmentvertical']
+        boolean: ['fluid', 'avatar', 'rounded', 'circular', 'bordered', 'centered'],
+        string: ['color', 'size', 'alignmentvertical']
     }
-    
-    function getContent(theProps, theIsEditMode){
+
+    function getContent(theProps, theIsEditMode) {
         var props = theProps;
         var tmpAtts = theProps.attributes;
         var tmpContent = [];
         var tmpNotes = '';
         var tmpCN = getClass(props, true);
-        if( tmpAtts.float  ){
-            if(theIsEditMode){
+        if (tmpAtts.float) {
+            if (theIsEditMode) {
                 tmpNotes = '** ' + tmpAtts.float
             } else {
                 tmpCN += ' ' + tmpAtts.float;
             }
-            
+
         }
-        var tmpEl = el('img',{className:tmpCN, src:tmpAtts.mediaURL});
-        if( theIsEditMode ){
-            if( tmpAtts.float ){
+        var tmpEl = el('img', { className: tmpCN, src: tmpAtts.mediaURL });
+        if (theIsEditMode) {
+            if (tmpAtts.float) {
                 var tmpSide = 'Floating Left';
-                if( tmpAtts.float == 'floated right' ){
+                if (tmpAtts.float == 'floated right') {
                     tmpSide = 'Floating Right';
                 }
-                tmpEl = el('div',{className:'ui segment basic pad0'},el('div',{className:'ui message fluid yellow'},tmpSide),tmpEl);
+                tmpEl = el('div', { className: 'ui segment basic pad0' }, el('div', { className: 'ui message fluid yellow' }, tmpSide), tmpEl);
             }
-            if( !(tmpAtts.mediaURL) ){
-//                tmpEl = el('div',{className:'ui message compact small orange'},'Select image on sidebar');
+            if (!(tmpAtts.mediaURL)) {
+                //                tmpEl = el('div',{className:'ui message compact small orange'},'Select image on sidebar');
 
-var onSelectImage = function( media ) {
-    tmpToSet = {};
-    tmpToSet[attNamesDef['mediaURL']] = media.url;
-    tmpToSet[attNamesDef['mediaID']] = media.id;
-    return theProps.setAttributes(tmpToSet);
-    
-};
+                var onSelectImage = function (media) {
+                    tmpToSet = {};
+                    tmpToSet[attNamesDef['mediaURL']] = media.url;
+                    tmpToSet[attNamesDef['mediaID']] = media.id;
+                    return theProps.setAttributes(tmpToSet);
 
-tmpEl = el( wp.blockEditor.MediaUpload, {
-    onSelect: onSelectImage,
-    type: 'image',
-    value: '',
-    render: function( obj ) {
-        
-        if( !theProps.attributes.mediaID ){
-            return el('div',{className:'pad2'},
-                el('div', {className:'ui button blue basic', onClick: obj.open}, 'Select Image')
-            )
-        } else {
-            return el('div',{className:'pad2'},
-            el('div', {className:'ui button blue basic', onClick: obj.open}, 'Replace'),
-            el('div', {className:'ui button blue basic', onClick: function(){}}, 'Remove'),                                            
-                el('div',{className:'pad2'}),
-                el('img',{className:'ui image rounded fluid', src:tmpAtts.mediaURL})
-            )
-        }
-    }
-} )  
+                };
 
+                tmpEl = el(wp.blockEditor.MediaUpload, {
+                    onSelect: onSelectImage,
+                    type: 'image',
+                    value: '',
+                    render: function (obj) {
+                        if (!theProps.attributes.mediaID) {
+                            return el('div', { className: 'pad2' },
+                                el('div', { className: 'ui button blue basic', onClick: obj.open }, 'Select Image')
+                            )
+                        } else {
+                            return el('div', { className: 'pad2' },
+                                el('div', { className: 'ui button blue basic', onClick: obj.open }, 'Replace'),
+                                el('div', { className: 'ui button blue basic', onClick: function () { } }, 'Remove'),
+                                el('div', { className: 'pad2' }),
+                                el('img', { className: 'ui image rounded fluid', src: tmpAtts.mediaURL })
+                            )
+                        }
+                    }
+                })
             }
             tmpContent.push(tmpEl)
         } else {
-            if( tmpAtts.url ){
-                var tmpOpts = {className:tmpCN,href:tmpAtts.url};
-                if( tmpAtts.urlopentab){
+            if (tmpAtts.url) {
+                var tmpOpts = { className: tmpCN, href: tmpAtts.url };
+                if (tmpAtts.urlopentab) {
                     tmpOpts.target = "_blank";
                     //--- Important, without this it shows 
                     tmpOpts.rel = "noopener";
                 }
-                tmpEl = el('a',tmpOpts,tmpEl);
+                tmpEl = el('a', tmpOpts, tmpEl);
             }
-            tmpContent  = tmpEl
+            tmpContent = tmpEl
         }
-        //tmpContent  = tmpEl
         return tmpContent;
     }
-    function getClass(theProps, theIsEditMode){
-        return BlockEditor.getStandardClass( 'ui image', tmpClassSpecs, theProps, theIsEditMode);
+    function getClass(theProps, theIsEditMode) {
+        return BlockEditor.getStandardClass('ui image', tmpClassSpecs, theProps, theIsEditMode);
     }
 
-    var attNamesDef = {mediaID:'mediaID',mediaURL:'mediaURL'};
-    wp.blocks.registerBlockType( info.category + '/' + info.name, {
+    var attNamesDef = { mediaID: 'mediaID', mediaURL: 'mediaURL' };
+    wp.blocks.registerBlockType(info.category + '/' + info.name, {
         title: info.title,
         icon: iconEl,
         category: info.category,
         example: info.example,
         attributes: info.atts,
-        edit: function ( props ) {
+        edit: function (props) {
             var tmpAtts = props.attributes;
-            var tmpCN = getClass(props, true);            
+            var tmpCN = getClass(props, true);
             var tmpContent = getContent(props, true);
-            // if( tmpAtts.size && tmpAtts.avatar){
-            //     tmpAtts.avatar = false;
-            // }
             var tmpStandardProperties = [
-                BlockEditor.getStandardProperty(props,attNamesDef, 'Image', 'image' ),
-                (tmpAtts.avatar || tmpAtts.rounded) ? '' : BlockEditor.getStandardProperty(props,'circular', 'Circular', 'checkbox' ),
-                (tmpAtts.avatar || tmpAtts.circular) ? '' : BlockEditor.getStandardProperty(props,'rounded', 'Rounded', 'checkbox' ),
-                BlockEditor.getStandardProperty(props,'bordered', 'Bordered', 'checkbox' ),
-                
-                (tmpAtts.avatar || tmpAtts.fluid) ? '' : BlockEditor.getStandardProperty(props,'size', 'Size', 'size' ),
-                (tmpAtts.avatar || tmpAtts.size) ? '' : BlockEditor.getStandardProperty(props,'fluid', 'Full width', 'checkbox' ),
-                (tmpAtts.size) ? '' : BlockEditor.getStandardProperty(props,'avatar', 'Show as icon', 'checkbox' ),
-                BlockEditor.getStandardProperty(props,'url', 'Target Content or Link', 'url' ),
-                !(tmpAtts.url) ? '' : BlockEditor.getStandardProperty(props,'urlopentab', 'Open link in new tab?', 'checkbox' ),                
-                BlockEditor.getStandardProperty(props,'spaced', 'Add spacing', 'checkbox' ),
+                BlockEditor.getStandardProperty(props, attNamesDef, 'Image', 'image'),
+                (tmpAtts.avatar || tmpAtts.rounded) ? '' : BlockEditor.getStandardProperty(props, 'circular', 'Circular', 'checkbox'),
+                (tmpAtts.avatar || tmpAtts.circular) ? '' : BlockEditor.getStandardProperty(props, 'rounded', 'Rounded', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'bordered', 'Bordered', 'checkbox'),
+                (tmpAtts.avatar || tmpAtts.fluid) ? '' : BlockEditor.getStandardProperty(props, 'size', 'Size', 'size'),
+                (tmpAtts.avatar || tmpAtts.size) ? '' : BlockEditor.getStandardProperty(props, 'fluid', 'Full width', 'checkbox'),
+                (tmpAtts.size) ? '' : BlockEditor.getStandardProperty(props, 'avatar', 'Show as icon', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'url', 'Target Content or Link', 'url'),
+                !(tmpAtts.url) ? '' : BlockEditor.getStandardProperty(props, 'urlopentab', 'Open link in new tab?', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'spaced', 'Add spacing', 'checkbox'),
             ];
             var tmpFormatProperties = [
-                (tmpAtts.float) ? '' : BlockEditor.getStandardProperty(props,'centered', 'Centered', 'checkbox' ),
-                (tmpAtts.centered) ? '' : BlockEditor.getStandardProperty(props,'float', 'Float', 'floatleftright' ),
-                BlockEditor.getStandardProperty(props,'alignmentvertical', 'Veritcal Alignment', 'alignmentvertical' ),
+                (tmpAtts.float) ? '' : BlockEditor.getStandardProperty(props, 'centered', 'Centered', 'checkbox'),
+                (tmpAtts.centered) ? '' : BlockEditor.getStandardProperty(props, 'float', 'Float', 'floatleftright'),
+                BlockEditor.getStandardProperty(props, 'alignmentvertical', 'Veritcal Alignment', 'alignmentvertical'),
             ];
             var tmpSidebarPanels = [
                 BlockEditor.getSidebarPanel('Image Options', tmpStandardProperties),
@@ -168,28 +161,10 @@ tmpEl = el( wp.blockEditor.MediaUpload, {
                 ]
             );
         },
- 
-        save: function ( props ) {
-            return getContent(props, false);            
-            //not using blockProps, need clean HTML
-            var tmpClass = getClass(props, false);
-            var tmpContent = getContent(props, false);
-            var tmpAtts = props.attributes;
-            var tmpEls = [
-                tmpAtts.text,
-                tmpContent
-            ]
-            if( tmpAtts.url ){
-                var tmpOpts = {className:tmpClass,href:tmpAtts.url};
-                if( tmpAtts.urlopentab){
-                    tmpOpts.target = "_blank";
-                }
-                return el('a',tmpOpts,tmpEls);
-            } else {
-                return el('div',{className:tmpClass},tmpEls);
-            }
 
+        save: function (props) {
+            return getContent(props, false);
         },
-    } );
-} )( window.wp, window.ActionAppCore );
+    });
+})(window.wp, window.ActionAppCore);
 
