1d69ee16e24e9e2c9ae73dfb1993791b9faa8370
Joseph Francis <hookedupjoe@gmail.com>
2022-08-15 22:26:15 -0400

    Exclude most blocks - todo=config

--- js/BlocksController.js
+++ js/BlocksController.js
@@ -114,7 +114,23 @@ if( tmpType == 'controls'){
             ThisApp.actions.updatePreview = function(){
                 ActAppBlocksController.loadFromMarkup();
             }
-            ActAppBlocksController.loadFromMarkup();
+            ActAppBlocksController.loadFromMarkup();            
+           
+            //--ToDo: Provide customize option for these
+            if( wp && wp.domReady){
+                wp.domReady( function () {
+                    //"core/file","core/html","core/paragraph","core/video","core/loginout"
+                    var tmpExludedBlocks = ["core/archives","core/avatar","core/block","core/calendar","core/categories","core/comment-author-name","core/comment-content","core/comment-date","core/comment-edit-link","core/comment-reply-link","core/comment-template","core/comments-pagination-next","core/comments-pagination-numbers","core/comments-pagination-previous","core/comments-pagination","core/comments-title","core/cover","core/gallery","core/home-link","core/image","core/latest-comments","core/latest-posts","core/navigation-link","core/navigation-submenu","core/navigation","core/page-list","core/pattern","core/post-author-biography","core/post-author","core/post-comments","core/post-comments-form","core/post-content","core/post-date","core/post-excerpt","core/post-featured-image","core/post-navigation-link","core/post-template","core/post-terms","core/post-title","core/query-no-results","core/query-pagination-next","core/query-pagination-numbers","core/query-pagination-previous","core/query-pagination","core/query-title","core/query","core/read-more","core/rss","core/search","core/shortcode","core/site-logo","core/site-tagline","core/site-title","core/social-link","core/tag-cloud","core/template-part","core/term-description","core/audio","core/button","core/buttons","core/code","core/column","core/columns","core/comments-query-loop","core/embed","core/freeform","core/group","core/heading","core/list","core/media-text","core/missing","core/more","core/nextpage","core/preformatted","core/pullquote","core/quote","core/separator","core/social-links","core/spacer","core/table","core/text-columns","core/verse"];
+                    for( var iPos in tmpExludedBlocks ){
+                        var tmpEntry = tmpExludedBlocks[iPos];
+                        try {
+                           wp.blocks.unregisterBlockType(tmpEntry);    
+                        } catch (error) {}
+                    }
+                } );
+            }
+
+            
         })
 
         
@@ -125,5 +141,6 @@ if( tmpType == 'controls'){
 
     //--- Initialize common block functionality for the editor
     init();
+
 
 } )( window.wp, window.ActionAppCore );
