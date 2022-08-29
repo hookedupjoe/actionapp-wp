a52acdcf052a84f620e351036f9654bc50d09381
Joseph Francis <hookedupjoe@gmail.com>
2022-08-29 09:55:23 -0400

    Swap initial page

--- apps/DevConsole/app/app.js
+++ apps/DevConsole/app/app.js
@@ -5,7 +5,7 @@
   try {
 
 
-    var tmpPageNames = ["Data","Designer","Admin","JSON", "Library"];
+    var tmpPageNames = ["Designer","Data","Admin","JSON", "Library"];
     var tmpPluginNames = [];
 
     if (typeof (window.cordova) == 'undefined') {
